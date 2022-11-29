896300b7dc9561e6bd7d65921073d3a91fd81d40
Gareth Searle <SEA19004582@btc.ac.uk>
2022-11-29 10:32:25 +0000

    Add project files.

---
+++ OOP - Shapes and Console Application/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Shapes
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                                          //"new" is an instance
+            Shapes square = new Shapes(4);//this is an object
+            Shapes triangle = new Shapes(3);
+            Shapes circle = new Shapes(1);
+
+            Console.WriteLine($"Squares have { square.NoOfSides } Sides.");
+            Console.WriteLine($"Triangles have { triangle.NoOfSides } Sides.");
+            Console.WriteLine($"Circles have { circle.NoOfSides } Sides.");
+            */
+
+            Product Banana = new Product( "Banana", 1.9f);
+            Product Chocolate = new Product("Chocolate", 3f);
+            Product TV = new Product();// uses default constructor
+            Product House = new Product("House");// uses constructor that only takes in name
+            /*
+            Console.WriteLine(Banana);
+            Console.WriteLine(Chocolate);
+            */
+
+            //"Product" is now a type and therefore we can use it in a list
+            List<Product> list = new List<Product>();
+            list.Add(Banana);
+            list.Add(Chocolate);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;//changes text color
+            //Console.BackgroundColor = ConsoleColor.Black;//changes Background colour
+
+            Animals Hippo = new Animals("Hippo", "Mammal", "Carnivoure", false );
+            Animals girrafe = new Animals("Girrafe", "Mammal", true);
+
+            List<Animals> Animals = new List<Animals>();
+            Animals.Add(Hippo);
+            Animals.Add(girrafe);
+
+            foreach (Animals a in Animals)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Customers customer1 = new Customers("Gareth", "searlegareth2004@gmail.com", "07957301514");
+            Customers customer2 = new Customers("Thomas", "searlethomas20071@gmail.com");
+
+            List<Customers> customerlist = new List<Customers>();
+            customerlist.Add(customer1);
+            customerlist.Add(customer2);
+
+            foreach (Customers c in customerlist)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
---
+++ OOP - Shapes and Console Application/Shapes.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    /*
+    internal class Shapes //This is a class
+    {
+        public int NoOfSides;//this is an object
+
+        public Shapes(int _noOfSides)
+        {
+            NoOfSides = _noOfSides;
+        }
+    }
+    */
+
+    internal class Product
+    {
+                   //Guid = Global Unique Identifier.  Generates a long number
+        public Guid ID;
+        public string Name;
+        public float Price;
+
+        //-----Consturctor Overloading-----
+        public Product()// Default Constructor - sets all parameters too defaults
+        {
+            ID = Guid.NewGuid();
+            Name = String.Empty;
+            Price = 0.00f;
+        }
+
+        public Product(string _name, float _price)// Constructor
+        {
+            ID = Guid.NewGuid();//generates a new Guid int
+            Name = _name;
+            Price = _price;
+        }
+
+        public Product(string _name)//only takes in name
+        {
+            ID = Guid.NewGuid();
+            Name = _name;
+            Price = 0.00f;
+        }
+
+
+        public override string ToString()
+        {
+            return $"ID: { ID }\nName: { Name }\nPrice: £{ Price }\n";
+        }
+    }
+
+
+
+
+    internal class Animals
+    {
+        public string AnimalName;
+        public string AnimalKingdom;
+        public string CarOmnHerb;
+        public bool Endangered;
+
+        public Animals()
+        {
+            AnimalName = String.Empty;
+            AnimalKingdom = String.Empty;
+            CarOmnHerb = String.Empty;
+            Endangered = false;
+        }
+
+        public Animals(string _animalName, string _animalKingdom, string _carOmnHerb, bool _endangered)
+        {
+            AnimalName = _animalName;
+            AnimalKingdom = _animalKingdom;
+            CarOmnHerb = _carOmnHerb;
+            Endangered = _endangered;
+        }
+        public Animals(string _animalName, string _animalKingdom, bool _endangered)
+        {
+            AnimalName = _animalName;
+            AnimalKingdom = _animalKingdom;
+            CarOmnHerb = String.Empty;
+            Endangered = _endangered;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: { AnimalName }\nKingdom: { AnimalKingdom }\nDiet: { CarOmnHerb }\nendangered: £{ Endangered }\n";
+        }
+    }
+
+
+    class Customers
+    {
+        public string CustomerName;
+        public string CustomerEmail;
+        public string CustomerPhone;
+
+        public Customers()
+        {
+            CustomerName = string.Empty;
+            CustomerEmail = string.Empty;
+            CustomerPhone = string.Empty;
+        }
+        public Customers(string _customerName, string _customerEmail, string _customerPhone)
+        {
+            CustomerName = _customerName;
+            CustomerEmail = _customerEmail;
+            CustomerPhone = _customerPhone;
+        }
+        public Customers(string _customerName, string _customerEmail)
+        {
+            CustomerName = _customerName;
+            CustomerEmail = _customerEmail;
+            CustomerPhone = String.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {CustomerName }\nEmail: { CustomerEmail }\nPhone: { CustomerPhone }\n";
+        }
+    }
+
+}
